d633036bf52d0258d9ead3c987e124df25a2a4f9
david <dleroys@umich.edu>
2021-04-02 19:03:27 -0400

    Decided to keep request and status codes in their own files

--- include/message.h
+++ include/message.h
@@ -4,20 +4,6 @@
 #include <cstdint>
 #include <vector>
 
-enum class Request_Code: uint16_t {
-    PING = 1,
-    GET_STATS = 2,
-    RESET_STATS = 3,
-    COMPRESS = 4
-};
-
-enum class Status_Code: uint16_t {
-    OK = 0,
-    UNKNOWN_ERROR = 1,
-    TOO_LARGE = 2,
-    UNSUPPORTED_TYPE = 3,
-};
-
 struct Header {
     uint32_t magic_number = 0x53545259;
     uint16_t payload_length;
